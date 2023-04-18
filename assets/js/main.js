7cddf57902be9864f78c083229d0817d46b464f2
leylahuseyn <127212987+leylahuseyn@users.noreply.github.com>
2023-04-18 17:53:52 +0400

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,98 @@
+// 1.Cevrenin sahesi -S = p*r*r (p=3)
+
+// var eded = prompt("eded daxil edin");
+// function areaOfCircle(r) {
+//    result =3*r*r;
+//    return result;
+// }
+// console.log(areaOfCircle(eded));
+
+// 2.Duzbucaqlinin sahesi - S = a*b
+
+// var eded1 = prompt("eded daxil edin");
+// var eded2 = prompt("eded daxil edin");
+// function areaOfRectangular(a,b) {
+//     result =a*b;
+//     return result;
+// }
+// console.log(areaOfRectangular(eded1,eded2));
+
+
+
+// 3.Duzbucaqli paralelpipedin tam sehtinin sahesi - S=2(a*b+a*c+b*c)
+
+// var eded1 = prompt("eded daxil edin");
+// var eded2 = prompt("eded daxil edin");
+// var eded3 = prompt("eded daxil edin");
+
+// function areaOfParallelepiped(a,b,c) {
+//     result =2*(a*b+a*c+b*c);
+//     return result;
+// }
+// console.log(areaOfParallelepiped(eded1,eded2,eded3));
+
+
+// 4.Ucbucaqlinin daxiline cekilmis cevrenin sahesi - S=p*r; p=(a+b+c)/2
+
+// var eded1 = Number(prompt("eded daxil edin"));
+// var eded2 = Number(prompt("eded daxil edin"));
+// var eded3 = Number(prompt("eded daxil edin"));
+// var eded4 = Number(prompt("eded daxil edin"));
+
+// function areaOfCircle(a,b,c,r) {
+//     result =((a+b+c)/2)*r
+//     return result;
+// }
+// console.log(areaOfCircle(eded1,eded2,eded3,eded4));
+
+
+// var eded1 = Number(prompt("eded daxil edin"));
+
+// function exam1(a) {
+//     if(a>9 && a<100){
+//      teklik =a%10;
+//      onluq =(a-teklik)/10;
+//     }
+//     result = onluq+" "+teklik;
+//     return result
+// }
+// console.log(exam1(eded1));
+
+
+// var eded1 = Number(prompt("eded daxil edin"));
+// var s=0
+// function exam1(number) {
+//     if (number >1) {
+//     while(number %2 ===0){
+//         number/=2;
+//         s++;
+//     }
+// }
+//     return number===1
+// }
+// console.log(exam1(eded1));
+// console.log(say);
+    
+    
+    
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
